17e249a871ea647983dff14cb63a433d025d9eec
Little Kong <cong.nguyen@dzones.vn>
2020-10-19 10:12:45 +0700

    updatte

--- cypress/def_func/vod.js
+++ cypress/def_func/vod.js
@@ -1,3 +1,6 @@
 Cypress.Commands.add('Play_AVOD_on_ribbon', () =>{
-    cy.get('#__next ').find('div:nth-child(6)').scrollIntoView().click()
+    cy
+        .get('#header')
+        //.contains('3a946288-62a9-4be0-85dc-d9da732df69e9f3e8b4c-abd8-46c7-9841-8178d84ac484')
+        .scrollIntoView({offset: {top: 3500, left: 0}, duration: 3000, easing: 'linear'})
 })
--- cypress/integration/trang_chu.js
+++ cypress/integration/trang_chu.js
@@ -6,11 +6,11 @@ import '../def_func/vod.js'
 
 
 describe('Check pop-up Đăng nhập', function () {
-  it('C1477-UI-Popup Đăng nhập từ icon user', function () {
-	cy.Homepage()
-	cy.Trigger_privilege_preview_panel()
-	cy.Check_hover_login_popup()
-  })
+//   it('C1477-UI-Popup Đăng nhập từ icon user', function () {
+// 	cy.Homepage()
+// 	cy.Trigger_privilege_preview_panel()
+// 	cy.Check_hover_login_popup()
+//   })
 
   it('C1478-UI-Popup Đăng nhập khi chọn AVOD', function () {
 	cy.Homepage()
