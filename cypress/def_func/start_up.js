7da1e29555588f7d479b1b0e60c30bdb1ffe1a9d
Little Kong <cong.nguyen@dzones.vn>
2020-10-21 18:13:54 +0700

    update

--- cypress/def_func/start_up.js
+++ cypress/def_func/start_up.js
@@ -1,5 +1,4 @@
 Cypress.Commands.add('Homepage', () => {
-    cy.viewport(1920, 1080)
     cy.visit('/', { onBeforeLoad: (win) => { win.sessionStorage.clear() } })
     cy.title().should('eq','VieON - Không Thể Rời Mắt')
 })
--- cypress/def_func/vod.js
+++ cypress/def_func/vod.js
@@ -1,6 +1,14 @@
-Cypress.Commands.add('Play_AVOD_on_ribbon', () =>{
+Cypress.Commands.add('Play_AVOD_on_top_10_ribbon', () =>{
     cy
-        .get('#header')
+        .get('#footer')
         //.contains('3a946288-62a9-4be0-85dc-d9da732df69e9f3e8b4c-abd8-46c7-9841-8178d84ac484')
-        .scrollIntoView({offset: {top: 3500, left: 0}, duration: 3000, easing: 'linear'})
+        .scrollIntoView({offset: {top: 1200, left: 0}, duration: 8000, timeout: 30000})
+
+    // cy
+    //     .get('#fcd453d0-e837-493f-85a5-83c8b759ceb01e013a15-766d-462c-a384-2fc1f85a51c6')
+    //     .click()
 })
+
+// Cypress.Commands.add('Login_required_pop_up', () =>{
+//     cy.get('.mask-inner > img').should
+// })
